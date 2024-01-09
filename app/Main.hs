880c8fdc3e6de7442b023939be43b3c218efca0a
Blake Oakley <brobersono@gmail.com>
2024-01-09 16:27:30 -0500

    use seed for deterministically random?

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,18 @@ module Main (main) where
 
 import Lib
 import CustomTypes
+import System.Random.Stateful
 import System.Random
+import Control.Monad (replicateM)
 import Initialization (initialize)
 
 main :: IO ()
 main = do
-    pop <- initialize
-    print pop
-
+    -- pop <- initialize
+    -- print pop
+    -- gen <- globalStdGen
+  let seed = 42
+      gen = mkStdGen seed
+  let rs =  randomRs (1,0) gen :: [Int]
+  print (take 3 rs)
 
